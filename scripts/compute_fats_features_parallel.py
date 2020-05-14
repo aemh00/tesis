3cc8059897d33f456ef49decbf4c907660e12d79
phuijse <pablo.huijse@gmail.com>
2020-05-13 23:56:57 -0400

    Faltaba pandas

--- scripts/compute_fats_features_parallel.py
+++ scripts/compute_fats_features_parallel.py
@@ -9,12 +9,11 @@ from features import compute_fats_features
 df_meta = parse_metadata(experiment="ALL", merge_subclasses=True)
 file_list = df_meta.index
 
-from joblib import Parallel, delayed
+from os.path import join
+import pandas as pd
+from joblib import Parallel, delayed, dump
 
 result = Parallel(n_jobs=10)(delayed(compute_fats_features)(batch_names) for batch_names in split_list_in_chunks(file_list, 100))
-
-from os.path import join
-from joblib import dump
 
 with open(join(vvv_path, "features/features_fats_all.pkl"), "wb") as f:
     dump(pd.concat(result), f)
